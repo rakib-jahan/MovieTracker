5a96d43054f82eb443cf94a842c82720ffcc777d
rakib-jahan <rakib.cse.sust@gmail.com>
2019-12-16 23:54:07 +0600

    DAL

---
+++ MovieTracker.DAL/MovieManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTracker.DAL
+{
+    public class MovieManager
+    {
+        MovieTrackerEntities movieTrackerEntities = null;
+
+        public MovieManager()
+        {
+            movieTrackerEntities = new MovieTrackerEntities();
+        }
+
+        public bool InsertMovie(Movie movie)
+        {
+            movieTrackerEntities.Movies.Add(movie);
+            return movieTrackerEntities.SaveChanges() > 0;
+        }
+
+        public bool DeleteUser(Movie movie)
+        {
+            movieTrackerEntities.Movies.Attach(movie);
+            movieTrackerEntities.Entry(movie).State = System.Data.Entity.EntityState.Deleted;
+            return movieTrackerEntities.SaveChanges() > 0;
+        }
+
+        public bool UpdateUser(Movie movie)
+        {
+            movieTrackerEntities.Movies.Attach(movie);
+            movieTrackerEntities.Entry(movie).State = System.Data.Entity.EntityState.Modified;
+            return movieTrackerEntities.SaveChanges() > 0;
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return movieTrackerEntities.Movies.ToList();
+        }
+    }
+}
--- MovieTracker/Models/Movie.cs
+++ MovieTracker/Models/Movie.cs
@@ -8,9 +8,11 @@ namespace MovieTracker.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
         public string Title { get; set; }
-        public string Year { get; set; }
+        public int Year { get; set; }
         public string Runtime { get; set; }
+        public int GenreId { get; set; }
         public string Genre { get; set; }
         public string Director { get; set; }
         public string Actors { get; set; }
