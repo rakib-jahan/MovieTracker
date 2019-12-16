7a162d29ca7da54db786f07b2ca1ff6555811063
Rakib Khan <rakib.cse.sust@adpeople.onmicrosoft.com>
2019-12-16 18:09:56 +0600

    movie class

---
+++ MovieTracker/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieTracker.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+        public string Metascore { get; set; }
+        public string ImdbRating { get; set; }
+        public bool IsAddedToWatchList { get; set; }
+    }
+}
